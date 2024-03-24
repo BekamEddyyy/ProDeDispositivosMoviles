685b12eb95c5e26a6062431ae1140e7e234b098b
BekamEddyyy <144222889+BekamEddyyy@users.noreply.github.com>
2024-03-23 22:03:34 -0500

    Update Ejercicio3.kt
    Agregando comentarios sobre la funcionabilidad del código

--- Lab01/Ejercicio3.kt
+++ Lab01/Ejercicio3.kt
@@ -5,21 +5,25 @@ fun main() {
     println("Ingrese la cantidad de alumnos en el salón:")
     val cantidadAlumnos = readLine()?.toInt()
 
+    // Verificar si la cantidad de alumnos es válida
     if (cantidadAlumnos == null || cantidadAlumnos <= 0) {
         println("Cantidad de alumnos no válida. Por favor, ingrese un número entero positivo.")
         return
     }
 
+    // Variables para almacenar la edad máxima, edad mínima y todas las edades
     var edadMaxima = Int.MIN_VALUE
     var edadMinima = Int.MAX_VALUE
     var todasLasEdades = ""
 
     println("Ingrese la edad de cada alumno:")
 
+    // Bucle para ingresar la edad de cada alumno
     for (i in 1..cantidadAlumnos) {
         print("Edad del alumno $i: ")
         val edad = readLine()?.toIntOrNull()
 
+        // Verificar si la edad ingresada es válida
         if (edad == null) {
             println("Edad no válida. Por favor, ingrese un número entero.")
             return
@@ -32,7 +36,8 @@ fun main() {
         if (edad < edadMinima) {
             edadMinima = edad
         }
-
+        
+        // Concatenar la edad a todasLasEdades
         todasLasEdades += "$edad"
         if (i < cantidadAlumnos) {
             todasLasEdades += ", "
