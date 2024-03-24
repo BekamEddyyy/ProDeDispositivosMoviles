a5346689be22d35cd9a9e90d78be90dcb4156f23
BekamEddyyy <144222889+BekamEddyyy@users.noreply.github.com>
2024-03-23 21:56:36 -0500

    Update Ejercicio1.cpp
    Agregando comentarios para explicar la funcionalidad del codigo

--- Lab01/Ejercicio1.cpp
+++ Lab01/Ejercicio1.cpp
@@ -1,4 +1,5 @@
-un calcularNivelRendimiento(puntuacion: Int): String {
+// Función para calcular el nivel de rendimiento basado en la puntuación
+fun calcularNivelRendimiento(puntuacion: Int): String {
     return when (puntuacion) {
         in 0..3 -> "Nivel de Rendimiento Bajo"
         in 4..6 -> "Nivel de Rendimiento Aceptable"
@@ -9,18 +10,22 @@ un calcularNivelRendimiento(puntuacion: Int): String {
     }
 }
 
+// Función para calcular el dinero recibido basado en la puntuación y el salario mensual
 fun calcularDineroRecibido(puntuacion: Int, salarioMensual: Int): Int {
     val dinero = salarioMensual * (puntuacion.toDouble() / 10)
     return dinero.toInt()
 }
 
 fun main() {
+    // Arrays de nombres, salarios mensuales y puntuaciones
     val usuarios = arrayOf("Juan", "María", "Pedro", "Camila", "Carlos") // Nombres de los usuarios
     val salariosMensuales = intArrayOf(10000, 12000, 9000, 8500, 7800) // Salarios mensuales de los usuarios
     val puntuaciones = intArrayOf(8, 7, 9, 7, 6) // Puntuaciones de los usuarios
 
     println("Resultados:")
+    // Iteración sobre los usuarios
     for (i in usuarios.indices) {
+        // Calcular el nivel de rendimiento y el dinero recibido para cada usuario
         val nivelRendimiento = calcularNivelRendimiento(puntuaciones[i])
         val dineroRecibido = calcularDineroRecibido(puntuaciones[i], salariosMensuales[i])
 
