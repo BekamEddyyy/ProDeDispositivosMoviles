f862b957220e8402a296d174199459f5119daa5a
BekamEddyyy <144222889+BekamEddyyy@users.noreply.github.com>
2024-03-23 22:09:42 -0500

    Update Ejercicio5.kt
    Agregando comentarios para la funcionalidad del código

--- Lab01/Ejercicio5.kt
+++ Lab01/Ejercicio5.kt
@@ -1,21 +1,25 @@
 import kotlin.random.Random
 
 fun main() {
+    //uso de la libreria random para imprimir un valor entre el 1 al 31
     val numeroAleatorio = Random.nextInt(1, 31)
     var intentos = 0
 
     println("¡Bienvenido al juego de Adivina el Número!")
     println("Tienes 5 intentos para adivinar un número entre 1 y 30.")
 
+    //los 5 intentos que tiene el usuario
     while (intentos < 5) {
         print("Intento ${intentos + 1}: Ingresa tu número: ")
         val numeroUsuario = readLine()?.toIntOrNull()
 
+        // Verificar si la entrada es válida
         if (numeroUsuario == null) {
             println("Entrada inválida. Por favor, ingresa un número entero.")
             continue
         }
 
+        // Verificar si el número del usuario es igual al número aleatorio
         if (numeroUsuario == numeroAleatorio) {
             println("¡Felicidades! ¡Has adivinado el número correctamente!")
             return
