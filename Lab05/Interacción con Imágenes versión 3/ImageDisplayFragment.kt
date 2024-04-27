f89610f22b3ab13aca7f602a5b2203b8da30c4a3
BekamEddyyy <144222889+BekamEddyyy@users.noreply.github.com>
2024-04-26 21:34:29 -0500

    Update ImageDisplayFragment.kt

--- Lab05/Interacción con Imágenes versión 3/ImageDisplayFragment.kt
+++ Lab05/Interacción con Imágenes versión 3/ImageDisplayFragment.kt
@@ -1,16 +1,27 @@
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
-import com.example.prueba1.R
+import com.example.prueba1.R // Replace with your package name
 
 class ImageDisplayFragment : Fragment() {
+
+    private lateinit var imageView: ImageView
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_image_display, container, false)
+        val view = inflater.inflate(R.layout.fragment_image_display, container, false)
+        imageView = view.findViewById(R.id.image_view)
+        return view
+    }
+
+    fun displayImage(bitmap: Bitmap) {
+        imageView.setImageBitmap(bitmap)
     }
 }
