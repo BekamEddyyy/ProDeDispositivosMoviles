d66a2d69b31d6e0f0c579084d97eae9cbf794025
BekamEddyyy <144222889+BekamEddyyy@users.noreply.github.com>
2024-04-26 21:41:36 -0500

    Update ImageSelectionFragment.kt

--- Lab05/Interacción con Imágenes versión 3/ImageSelectionFragment.kt
+++ Lab05/Interacción con Imágenes versión 3/ImageSelectionFragment.kt
@@ -1,4 +1,3 @@
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,23 +9,18 @@ import androidx.fragment.app.Fragment
 import com.example.prueba1.R
 
 class ImageSelectionFragment : Fragment() {
-    private lateinit var spinner: Spinner
-    private var selectedImage: String = ""
-    private lateinit var listener: OnImageSelectedListener
 
     interface OnImageSelectedListener {
         fun onImageSelected(imageName: String)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    private lateinit var spinner: Spinner
+    private var listener: OnImageSelectedListener? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_image_selection, container, false)
 
         spinner = view.findViewById(R.id.spinner_images)
-
         val adapter = ArrayAdapter.createFromResource(
             requireContext(),
             R.array.image_names,
@@ -37,8 +31,7 @@ class ImageSelectionFragment : Fragment() {
 
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
-                selectedImage = parent.getItemAtPosition(position).toString()
-                listener.onImageSelected(selectedImage)
+                listener?.onImageSelected(parent.getItemAtPosition(position).toString())
             }
 
             override fun onNothingSelected(parent: AdapterView<*>) {}
@@ -47,12 +40,8 @@ class ImageSelectionFragment : Fragment() {
         return view
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnImageSelectedListener) {
-            listener = context
-        } else {
-            throw RuntimeException("$context must implement OnImageSelectedListener")
-        }
+    fun setOnImageSelectedListener(listener: OnImageSelectedListener) {
+        this.listener = listener
     }
 }
+
