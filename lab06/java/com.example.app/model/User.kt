2236d768148fc89d12c91d3b4538c3fbe992bc93
BekamEddyyy <144222889+BekamEddyyy@users.noreply.github.com>
2024-05-31 07:24:54 -0500

    Update User.kt

--- lab06/java/com.example.app/model/User.kt
+++ lab06/java/com.example.app/model/User.kt
@@ -4,10 +4,11 @@ package com.example.app.model
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
+// Clase de datos User que representa a un usuario en la aplicación
 @Parcelize
 data class User(
-    val id: Int,
-    var name: String,
-    var email: String,
-    var imageResourceId: Int
-) : Parcelable
+    val id: Int, // ID único del usuario
+    var name: String, // Nombre del usuario
+    var email: String, // Correo electrónico del usuario
+    var imageResourceId: Int // ID del recurso de imagen del usuario
+) : Parcelable // Implementa Parcelable para permitir pasar objetos User entre componentes de Android
